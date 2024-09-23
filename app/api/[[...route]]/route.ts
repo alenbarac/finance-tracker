f5fa6818ad936086d9eb43d0c57c984f3ac4bd7d
Alen <info@alenbarac.com>
2024-09-23 14:26:10 +0200

    honojs clerk middleware added for api routes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,29 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
-  })
-})
+app.get(
+  '/hello',
+
+  clerkMiddleware(),
+
+  (c) => {
+    const auth = getAuth(c)
+    if (!auth?.userId) {
+      return c.json({
+        message: 'Hello Next.js! You are not logged in.',
+      })
+    }
+    return c.json({
+      message: 'Hello Next.js!',
+      userId: auth.userId,
+    })
+  }
+)
 
 export const GET = handle(app)
 export const POST = handle(app)
